1509e38adf0b7423d0bf6a0a4d46299de7743dc2
Dave Kretonen <140484172+SynthouS@users.noreply.github.com>
2025-02-23 11:36:21 +0500

    replaced conf path

--- src/config.h
+++ src/config.h
@@ -1,13 +1,15 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#define CONFIG_PATH "/etc/tuskwm.conf"
+
 typedef struct {
     int width;
     int height;
     char* wallpaper_path;
 } Config;
 
-Config load_config(const char* config_path);
+Config load_config();
 void free_config(Config* config);
 
 #endif
